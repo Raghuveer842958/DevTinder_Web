f8e36c50c9d759e0e6bd77d83c252b40eece960e
Raghuveer Chauhan <raghuveerchauhan7787@gmail.com>
2025-01-23 14:52:25 +0530

    Blue Tick Bug Fixing

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://dev-tinder-backend-2.onrender.com";
 export const LOCAL_URL = "http://localhost:8080";
-export const BASE_URL2 = "https://dev-tinder-backend-p9wn.onrender.com";
+export const HOISTING_URL = "https://dev-tinder-backend-2.onrender.com";
 export const PROFILE_URL="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+// https://dev-tinder-backend-2.onrender.com
