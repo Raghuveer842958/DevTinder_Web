fadbb6196498b091f56d04abf593a341fd7f987f
Raghuveer Chauhan <raghuveerchauhan7787@gmail.com>
2025-01-30 17:48:39 +0530

    Setting origin

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "dev-tinder-backend-iota.vercel.app";
+export const BASE_URL = "http://localhost:8080";
 export const LOCAL_URL = "http://localhost:8080";
 export const HOISTING_URL = "dev-tinder-backend-iota.vercel.app";
 export const PROFILE_URL =
